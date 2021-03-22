d7dc1aab37a7f589bc7fa0d47e867c5275f4f2cc
Jambit0S <lnmohaddd@gmail.com>
2021-03-22 14:47:52 +0300

    Добавьте файлы проекта.

---
+++ taskTest.cs
@@ -0,0 +1,17 @@
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Tasks.v1;
+using Google.Apis.Tasks.v1.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+//новы ехе файл с консолью
+public class TodoGoogle
+{
+    
+}
